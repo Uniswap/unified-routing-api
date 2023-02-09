0feec3af8dae4e9fc582e28dc128f3e43d3c43c5
ConjunctiveNormalForm <zhiyuan.zach.yang@gmail.com>
2023-02-09 17:52:27 -0500

    use WRAPPED_NATIVE_CURRENCY from sor package

--- lib/providers/transformers/RequestTransformers/RouteBackToEthRequestTransformer.ts
+++ lib/providers/transformers/RequestTransformers/RouteBackToEthRequestTransformer.ts
@@ -1,12 +1,18 @@
 import { Protocol } from '@uniswap/router-sdk';
-import Logger from 'bunyan';
-
 import { TradeType } from '@uniswap/sdk-core';
+import Logger from 'bunyan';
 import { parseEther } from 'ethers/lib/utils';
+
+import { ID_TO_CHAIN_ID, WRAPPED_NATIVE_CURRENCY } from '@uniswap/smart-order-router';
 import { RequestTransformer } from '..';
 import { QuoteRequest } from '../../../entities';
 import { ClassicRequest } from '../../../entities/request/ClassicRequest';
+import { RoutingType } from '../../../entities/request/index';
+import { RequestByRoutingType } from '../../../handlers/quote/handler';
 
+/*
+ * adds a synthetic classic request to check if the output token has route back to ETH
+ */
 export class RouteBackToEthTransformer implements RequestTransformer {
   private log: Logger;
 
@@ -14,13 +20,21 @@ export class RouteBackToEthTransformer implements RequestTransformer {
     this.log = _log.child({ quoter: 'RouteBackToEthTransformer' });
   }
   transform(requests: QuoteRequest[], gasPriceWei: string): QuoteRequest[] {
+    const requestByRoutingType: RequestByRoutingType = {};
+    requests.forEach((r) => (requestByRoutingType[r.routingType] = r));
+
+    if (!requestByRoutingType[RoutingType.DUTCH_LIMIT]) {
+      this.log.info('UniswapX not requested, skipping transformer');
+      return requests;
+    }
+
     const synthClassicRequest = new ClassicRequest(
       {
         ...requests[0].info,
         type: TradeType.EXACT_OUTPUT,
         tokenIn: requests[0].info.tokenOut,
         amount: parseEther('1'),
-        tokenOut: 'ETH',
+        tokenOut: WRAPPED_NATIVE_CURRENCY[ID_TO_CHAIN_ID(requests[0].info.tokenOutChainId)].address,
       },
       {
         protocols: [Protocol.MIXED, Protocol.V2, Protocol.V3],
--- test/providers/transformers/RequestTransformers/RouteBackToEthTransformer.test.ts
+++ test/providers/transformers/RequestTransformers/RouteBackToEthTransformer.test.ts
@@ -1,8 +1,10 @@
 import { TradeType } from '@uniswap/sdk-core';
 import Logger from 'bunyan';
 import { ethers } from 'ethers';
+
+import { ChainId, WRAPPED_NATIVE_CURRENCY } from '@uniswap/smart-order-router';
 import { RouteBackToEthTransformer } from '../../../../lib/providers/transformers/RequestTransformers/RouteBackToEthRequestTransformer';
-import { QUOTE_REQUEST_CLASSIC } from '../../../utils/fixtures';
+import { QUOTE_REQUEST_CLASSIC, QUOTE_REQUEST_DL } from '../../../utils/fixtures';
 
 describe('RouteBackToEthTransformer', () => {
   const logger = Logger.createLogger({ name: 'test' });
@@ -10,13 +12,13 @@ describe('RouteBackToEthTransformer', () => {
 
   const transformer = new RouteBackToEthTransformer(logger);
 
-  it('adds a synthetic classic request', async () => {
-    const requests = transformer.transform([QUOTE_REQUEST_CLASSIC], '1000000000');
+  it('adds a synthetic classic request when UniswapX requested', async () => {
+    const requests = transformer.transform([QUOTE_REQUEST_DL], '1000000000');
     expect(requests.length).toEqual(2);
     expect(requests[1]).toMatchObject({
       info: {
         tokenIn: QUOTE_REQUEST_CLASSIC.info.tokenOut,
-        tokenOut: 'ETH',
+        tokenOut: WRAPPED_NATIVE_CURRENCY[ChainId.MAINNET].address,
         type: TradeType.EXACT_OUTPUT,
         amount: ethers.utils.parseEther('1'),
       },
@@ -25,5 +27,10 @@ describe('RouteBackToEthTransformer', () => {
         gasPriceWei: '1000000000',
       },
     });
+  });
+
+  it('does not add a synthetic classic request when UniswapX not requested', async () => {
+    const requests = transformer.transform([QUOTE_REQUEST_CLASSIC], '1000000000');
+    expect(requests.length).toEqual(1);
   });
 });
