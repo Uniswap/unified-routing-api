a4fe02edb0b2060f14b605820fb9f9ba64df5da8
Eric Zhong <eric.zhong@uniswap.org>
2023-04-04 21:43:57 -0400

    Add weth wrap gas cost

--- lib/constants.ts
+++ lib/constants.ts
@@ -5,5 +5,4 @@ export const DEFAULT_EXCLUSIVE_PERIOD_SECS = 16;
 export const DEFAULT_SLIPPAGE_TOLERANCE = '0.5'; // 0.5%
 export const HUNDRED_PERCENT = BigNumber.from(100_00); // 100.00%
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
-// TODO: replace with the real WETH wrap gas cost
-export const WETH_WRAP_GAS = 10000;
+export const WETH_WRAP_GAS = 27938; // 27,938 warm deposit, 45,038 cold deposit
