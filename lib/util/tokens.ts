145894cc54fd6ea5af49c6730c86c7b2d1a8551d
dannydaniil <danny.daniil@uniswap.org>
2023-05-19 13:52:07 -0400

    token provider

--- lib/util/tokens.ts
+++ lib/util/tokens.ts
@@ -1,11 +1,58 @@
 import DEFAULT_TOKEN_LIST from '@uniswap/default-token-list';
-import { CachingTokenListProvider, ChainId, NodeJSCache } from '@uniswap/smart-order-router';
+import {
+  CachingTokenListProvider,
+  CachingTokenProviderWithFallback,
+  ChainId,
+  NodeJSCache,
+  TokenProvider,
+  UniswapMulticallProvider,
+} from '@uniswap/smart-order-router';
 import { ethers } from 'ethers';
 import NodeCache from 'node-cache';
 import { ValidationError } from './errors';
+import { Token } from '@uniswap/sdk-core'
+
+export const ARBITRUM_TIMEOUT = 8000;
+export const DEFAULT_TIMEOUT = 5000;
 
 export const getTokenListProvider = (id: ChainId) => {
   return new CachingTokenListProvider(id, DEFAULT_TOKEN_LIST, new NodeJSCache(new NodeCache()));
+};
+
+export const getTokenProvider = (chainId: ChainId) => {
+  let timeout: number;
+  switch (chainId) {
+    case ChainId.ARBITRUM_ONE:
+    case ChainId.ARBITRUM_RINKEBY:
+      timeout = ARBITRUM_TIMEOUT;
+      break;
+    default:
+      timeout = DEFAULT_TIMEOUT;
+      break;
+  }
+
+  const rpcUrl = process.env[`RPC_${chainId}`];
+  if (!rpcUrl) {
+    throw new Error(`RPC_${chainId} is not defined`);
+  }
+
+  const provider = new ethers.providers.JsonRpcProvider(
+    {
+      url: rpcUrl,
+      timeout,
+    },
+    chainId
+  );
+
+  const tokenCache = new NodeJSCache<Token>(new NodeCache({ stdTTL: 3600, useClones: false }));
+
+  const multicall2Provider = new UniswapMulticallProvider(chainId, provider, 375_000);
+  return new CachingTokenProviderWithFallback(
+    chainId,
+    tokenCache,
+    getTokenListProvider(chainId),
+    new TokenProvider(chainId, multicall2Provider)
+  );
 };
 
 export const getAddress = async (id: ChainId, symbolOrAddress: string): Promise<string> => {
