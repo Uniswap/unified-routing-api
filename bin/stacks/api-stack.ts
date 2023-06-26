b22d9e12eb659c892cc20f641f0bb8e6479ee219
Zach Yang <zhiyuan.zach.yang@gmail.com>
2023-06-26 12:47:01 -0400

    add internal api key (#169)

--- bin/stacks/api-stack.ts
+++ bin/stacks/api-stack.ts
@@ -45,6 +45,7 @@ export class APIStack extends cdk.Stack {
     name: string,
     props: cdk.StackProps & {
       provisionedConcurrency: number;
+      internalApiKey?: string;
       throttlingOverride?: string;
       chatbotSNSArn?: string;
       stage: STAGE;
@@ -52,7 +53,7 @@ export class APIStack extends cdk.Stack {
     }
   ) {
     super(parent, name, props);
-    const { provisionedConcurrency, stage, chatbotSNSArn } = props;
+    const { provisionedConcurrency, stage, chatbotSNSArn, internalApiKey } = props;
 
     /*
      *  API Gateway Initialization
@@ -113,6 +114,27 @@ export class APIStack extends cdk.Stack {
               forwardedIpConfig: {
                 headerName: 'X-Forwarded-For',
                 fallbackBehavior: 'MATCH',
+              },
+              scopeDownStatement: {
+                notStatement: {
+                  statement: {
+                    byteMatchStatement: {
+                      fieldToMatch: {
+                        singleHeader: {
+                          name: 'x-api-key',
+                        },
+                      },
+                      positionalConstraint: 'EXACTLY',
+                      searchString: internalApiKey,
+                      textTransformations: [
+                        {
+                          type: 'NONE',
+                          priority: 0,
+                        },
+                      ],
+                    },
+                  },
+                },
               },
             },
           },
@@ -475,7 +497,7 @@ export class APIStack extends cdk.Stack {
       apiAlarm4xxSev3.addAlarmAction(new cdk.aws_cloudwatch_actions.SnsAction(chatBotTopic));
       apiAlarmLatencySev2.addAlarmAction(new cdk.aws_cloudwatch_actions.SnsAction(chatBotTopic));
       apiAlarmLatencySev3.addAlarmAction(new cdk.aws_cloudwatch_actions.SnsAction(chatBotTopic));
-      
+
       percent5XXByChainAlarm.forEach((alarm) => {
         alarm.addAlarmAction(new cdk.aws_cloudwatch_actions.SnsAction(chatBotTopic));
       });
