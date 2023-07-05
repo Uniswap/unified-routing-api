65d27c50d6c2fc528f2275fa374610fd2420eaca
marktoda <toda.mark@gmail.com>
2023-07-05 13:22:08 -0700

    feat: temporarily remove wrap gas cost (#176)
    To allow for easier testing of ETH input
    TODO: REVERT!

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,7 +3,8 @@ import { BigNumber } from 'ethers';
 export const DEFAULT_SLIPPAGE_TOLERANCE = '0.5'; // 0.5%
 export const BPS = 10000; // 100.00%
 export const NATIVE_ADDRESS = '0x0000000000000000000000000000000000000000';
-export const WETH_WRAP_GAS = 27938; // 27,938 warm deposit, 45,038 cold deposit
+// export const WETH_WRAP_GAS = 27938; // 27,938 warm deposit, 45,038 cold deposit
+export const WETH_WRAP_GAS = 0; // TODO: remove
 export const WETH_UNWRAP_GAS = 36000;
 export const DEFAULT_EXCLUSIVITY_OVERRIDE_BPS = BigNumber.from(100); // non-exclusive fillers must override price by this much
 export const GOUDA_BASE_GAS = 120000; // base gas overhead for filling an order through Gouda
