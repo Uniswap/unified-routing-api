9fdc3ffc54e828cac5e141ce560e8ffb6fb60133
Eric Zhong <eric.zhong@uniswap.org>
2023-07-06 15:46:23 -0400

    fix unused import

--- lib/entities/request/ClassicRequest.ts
+++ lib/entities/request/ClassicRequest.ts
@@ -3,7 +3,6 @@ import { BigNumber } from 'ethers';
 
 import { defaultRequestKey, QuoteRequest, QuoteRequestInfo } from '.';
 import { RoutingType } from '../../constants';
-import { DutchRequest } from './DutchRequest';
 
 export interface ClassicConfig {
   protocols?: Protocol[];
