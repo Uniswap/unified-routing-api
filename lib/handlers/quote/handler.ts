a2e13fe3ae993cde93b59cde91710f8e5cb07b8a
Eric Zhong <eric.zhong@uniswap.org>
2023-07-06 17:53:09 -0400

    filter out 0 value quotes in getQutoes

--- lib/handlers/quote/handler.ts
+++ lib/handlers/quote/handler.ts
@@ -181,7 +181,7 @@ export async function getQuotes(quoterByRoutingType: QuoterByRoutingType, reques
       return quoter.quote(request);
     })
   );
-  return quotes.filter((q): q is Quote => !!q);
+  return quotes.filter((q): q is Quote => !!q && (!q.amountOut.eq(0) || q.amountIn.eq(0)));
 }
 
 // determine and return the "best" quote of the given list
