d090ebe258537448936cb9e4f33aa37f1553d4a3
Eric Zhong <eric.zhong@uniswap.org>
2023-07-12 19:42:41 -0400

    bump the weth wrap gas, add comment

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,8 +3,14 @@ import { BigNumber } from 'ethers';
 export const DEFAULT_SLIPPAGE_TOLERANCE = '0.5'; // 0.5%
 export const BPS = 10000; // 100.00%
 export const NATIVE_ADDRESS = '0x0000000000000000000000000000000000000000';
-export const WETH_WRAP_GAS = 27938; // 27,938 warm deposit, 45,038 cold deposit
+
+// Because we don't natively support ETH input and require users to wrap their ETH before swapping,
+// the user experience is significantly worse (requiring 3 user interactions).
+// Thus, we add a negative bias towards ETH input trades through adding a gas adjustment in an effort
+// to route users towards classic swaps unless UniswapX is significantly better.
+export const WETH_WRAP_GAS = 27938 * 3; // 27,938 warm deposit, 45,038 cold deposit
 export const WETH_UNWRAP_GAS = 36000;
+
 export const DEFAULT_EXCLUSIVITY_OVERRIDE_BPS = BigNumber.from(100); // non-exclusive fillers must override price by this much
 export const UNISWAPX_BASE_GAS = 250000; // base gas overhead for filling an order through Gouda
 
