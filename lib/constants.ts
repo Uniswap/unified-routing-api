c56f2848766ad1665aedd70ee338338bcb8aeb63
Eric Zhong <eric.zhong@uniswap.org>
2023-07-12 14:43:49 -0400

    add back weth wrap gas

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,8 +3,7 @@ import { BigNumber } from 'ethers';
 export const DEFAULT_SLIPPAGE_TOLERANCE = '0.5'; // 0.5%
 export const BPS = 10000; // 100.00%
 export const NATIVE_ADDRESS = '0x0000000000000000000000000000000000000000';
-// export const WETH_WRAP_GAS = 27938; // 27,938 warm deposit, 45,038 cold deposit
-export const WETH_WRAP_GAS = 0; // TODO: remove
+export const WETH_WRAP_GAS = 27938; // 27,938 warm deposit, 45,038 cold deposit
 export const WETH_UNWRAP_GAS = 36000;
 export const DEFAULT_EXCLUSIVITY_OVERRIDE_BPS = BigNumber.from(100); // non-exclusive fillers must override price by this much
 export const UNISWAPX_BASE_GAS = 250000; // base gas overhead for filling an order through Gouda
