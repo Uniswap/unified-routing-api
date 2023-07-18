77656fb11b0cf2199ffe86ba50c89ba2a0f60cf2
Riley Campbell <rileydcampbell@berkeley.edu>
2023-07-18 23:39:32 +0200

    feat: remove seploia alarm

--- bin/stacks/api-stack.ts
+++ bin/stacks/api-stack.ts
@@ -28,6 +28,7 @@ export const CHAINS_NOT_ALARMED = new Set<ChainId>([
   ChainId.OPTIMISM_GOERLI,
   ChainId.GOERLI,
   ChainId.POLYGON_MUMBAI,
+  ChainId.SEPOLIA,
 ]);
 
 const ALL_ALARMED_CHAINS = _.filter(ALL_SUPPORTED_CHAINS, (c) => !CHAINS_NOT_ALARMED.has(c));
