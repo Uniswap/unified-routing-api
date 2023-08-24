4b0fb478ea91eb23b2db00a1e2bcb7c0beb5eabe
marktoda <toda.mark@gmail.com>
2023-08-24 22:02:14 +0200

    Revert "feat: try 512 memory (#240)" (#242)
    This reverts commit f24020dcadf956016e959ef6a325ac2511200f77.

--- bin/stacks/api-stack.ts
+++ bin/stacks/api-stack.ts
@@ -176,7 +176,7 @@ export class APIStack extends cdk.Stack {
       runtime: aws_lambda.Runtime.NODEJS_16_X,
       entry: path.join(__dirname, '../../lib/handlers/index.ts'),
       handler: 'quoteHandler',
-      memorySize: 512,
+      memorySize: 1024,
       bundling: {
         minify: true,
         sourceMap: true,
