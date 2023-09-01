a2f2328575cf2b2dc0fc0b6fad88dca6a02125b2
marktoda <toda.mark@gmail.com>
2023-09-01 20:32:45 +0200

    feat: increase WETH wrap overhead (#248)
    Basically to account for the UX cost of wrapping weth

--- lib/constants.ts
+++ lib/constants.ts
@@ -9,7 +9,7 @@ export const NATIVE_ADDRESS = '0x0000000000000000000000000000000000000000';
 // Thus, we add a negative bias towards ETH input trades through adding a gas adjustment in an effort
 // to route users towards classic swaps unless UniswapX is significantly better.
 export const WETH_WRAP_GAS = 27938 * 3; // 27,938 warm deposit, 45,038 cold deposit
-export const WETH_WRAP_GAS_ALREADY_APPROVED = 27938;
+export const WETH_WRAP_GAS_ALREADY_APPROVED = 27938 * 2;
 export const WETH_UNWRAP_GAS = 36000;
 
 export const DEFAULT_EXCLUSIVITY_OVERRIDE_BPS = BigNumber.from(100); // non-exclusive fillers must override price by this much
