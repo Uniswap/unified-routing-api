13212711d62aaf37c7f0cc1aff97fc29a1d77cda
Miguel Cervera <miguelcerverac@gmail.com>
2023-10-12 10:04:27 -0700

    Wire down TestPortion option (#288)
    * Write down secrets
    
    * Pass force portion using global
    
    * set force portion global
    
    * test for portion
    
    * newline
    
    * added to gouda tests
    
    * simplify boolean logic
    
    * comment on why bypass cache
    
    * tests
    
    * bypass cache set too
    
    * fix test for portion cache bypass
    
    * fix tests
    
    * assert no cache entry

--- lib/fetchers/PortionFetcher.ts
+++ lib/fetchers/PortionFetcher.ts
@@ -6,6 +6,7 @@ import { DEFAULT_NEGATIVE_CACHE_ENTRY_TTL, DEFAULT_POSITIVE_CACHE_ENTRY_TTL, ura
 import axios from '../providers/quoters/helpers';
 import { log } from '../util/log';
 import { metrics } from '../util/metrics';
+import { forcePortion } from '../util/portion';
 
 export enum PortionType {
   Flat = 'flat',
@@ -65,7 +66,9 @@ export class PortionFetcher {
       return GET_NO_PORTION_RESPONSE;
     }
 
-    const portionFromCache = this.portionCache.get<GetPortionResponse>(
+    // We bypass the cache if `forcePortion` is true.
+    // We do it to avoid cache conflicts since `forcePortion` is only for testing purposes.
+    const portionFromCache = !forcePortion && this.portionCache.get<GetPortionResponse>(
       this.PORTION_CACHE_KEY(tokenInChainId, tokenInAddress, tokenOutChainId, tokenOutAddress)
     );
 
@@ -90,11 +93,15 @@ export class PortionFetcher {
       metrics.putMetric(`PortionFetcherSuccess`, 1);
       metrics.putMetric(`PortionFetcherCacheMiss`, 1);
 
-      this.portionCache.set<GetPortionResponse>(
-        this.PORTION_CACHE_KEY(tokenInChainId, tokenInAddress, tokenOutChainId, tokenOutAddress),
-        portionResponse.data,
-        portionResponse.data.portion ? this.positiveCacheEntryTtl : this.negativeCacheEntryTtl
-      );
+      // We bypass the cache if `forcePortion` is true.
+      // We do it to avoid cache conflicts since `forcePortion` is only for testing purposes.
+      if (!forcePortion) {
+        this.portionCache.set<GetPortionResponse>(
+          this.PORTION_CACHE_KEY(tokenInChainId, tokenInAddress, tokenOutChainId, tokenOutAddress),
+          portionResponse.data,
+          portionResponse.data.portion ? this.positiveCacheEntryTtl : this.negativeCacheEntryTtl
+        );
+      }
 
       return portionResponse.data;
     } catch (e) {
