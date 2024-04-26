7ca1577afc38454cc8abcd1b6e19558eff29f8ef
Cody Born <cody.born@uniswap.org>
2024-04-26 10:02:55 -0400

    Improve Dependency Tracking (#403)

--- lib/fetchers/Permit2Fetcher.ts
+++ lib/fetchers/Permit2Fetcher.ts
@@ -4,6 +4,7 @@ import { ethers, providers } from 'ethers';
 import PERMIT2_CONTRACT from '../abis/Permit2.json';
 import { log } from '../util/log';
 import { metrics } from '../util/metrics';
+import { Unit } from 'aws-embedded-metrics';
 
 export class Permit2Fetcher {
   public readonly permit2Address: string;
@@ -27,10 +28,16 @@ export class Permit2Fetcher {
     let allowance = undefined;
     metrics.putMetric(`Permit2FetcherRequest`, 1);
     try {
+      const beforePermitCheck = Date.now();
       const rpcProvider = this.chainIdRpcMap.get(chainId);
       if (!rpcProvider) throw new Error(`No rpc provider found for chain: ${chainId}`);
       allowance = await this.permit2.connect(rpcProvider).allowance(ownerAddress, tokenAddress, spenderAddress);
-      metrics.putMetric(`Permit2FetcherSuccess`, 1);
+      metrics.putMetric(`Permit2FetcherSuccess`, 1);    metrics.putMetric(
+        `Latency-Permit2Fetcher-ChainId${chainId}`,
+        Date.now() - beforePermitCheck,
+        Unit.Milliseconds
+      );
+  
     } catch (e) {
       log.error(e, 'Permit2FetcherErr');
       metrics.putMetric(`Permit2FetcherErr`, 1);
